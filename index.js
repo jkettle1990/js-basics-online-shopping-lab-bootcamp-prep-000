333bc780f56a5fca086d875b243c6fa8e4d08e0e
Josh <joshuakettle1990@gmail.com>
2017-10-31 16:53:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var itemPrice = Math.random() * 100;
- cart.push(new Object(item: itemPrice));
+ cart.push(new Object(item : itemPrice));
  return `${item} has been added to your cart.`
  return cart;
 }
